86e233461babaeca9314837ea58417ca7500bb47
jjmuesesq <jjmuesesq@unal.edu.co>
2024-04-17 01:23:20 -0500

    :sparkles: Introducción a la consola

--- 01-fundamentos/app.js
+++ 01-fundamentos/app.js
@@ -4,11 +4,25 @@
 
 let a = 10,
     b = 20,
-    c = 10,
-    d = 10,
+    c = 'Hola ',
+    d = 'Spiderman',
     x = a + b;
+const saludo = c + d;
+console.log('%c Mis variables', 'color:blue; font-weight:bold');
+console.log( x );
+console.warn( x );
+console.error( x );
+console.info( x );
 
-console.log(x);
+console.log( 'a', a );
+console.log( 'b', b );
+console.log( 'c', c );
+
+console.log( {a} );
+console.log( {b} );
+console.log( {c} );
+
+console.table({a, b, c, d, x})
 
 //var b = 10; // forma antigua de crear variables
 // const c = 10;
