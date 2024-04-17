6fd80d5e4c29299d6d567316646cd84864ef9335
jjmuesesq <jjmuesesq@unal.edu.co>
2024-04-16 22:48:56 -0500

    :sparkles: Introducción a variables y comentarios

--- 01-fundamentos/app.js
+++ 01-fundamentos/app.js
@@ -1,1 +1,27 @@
-console.log(console.log('Hola mundo'));
+// console.log(console.log('Hola mundo'));
+
+// VARIABLES
+
+let a = 10,
+    b = 20,
+    c = 10,
+    d = 10,
+    x = a + b;
+
+console.log(x);
+
+//var b = 10; // forma antigua de crear variables
+// const c = 10;
+
+// c = 24; // no se puede hacer
+// a = 20; 
+// b = 30;
+
+
+// VERSIONES DE JAVA SCRIPT
+// 1996 live script a javascript (standar)
+// 1997 ES1 ECMAScript 1
+// 2009 ES5 ECMAScript 5 nuevas caracteristicas -> soporta en todos los navegadores web
+// 2015 ES6 ECMAScript 2015 actualización mas grande hasta el momento -> soporta la mayoria de navegadores modernos pero no todos los navegadores web
+
+// polyfill -> codigo que provee funcionamiento de una nueva caracteristica de JavaScript ES6 en versiones viejas como ES5
