b5c95f941e6dec869e083165e92273c4f798e246
jjmuesesq <jjmuesesq@unal.edu.co>
2024-04-18 13:45:44 -0500

    :sparkles: Prompt, confirm y alert

--- 01-fundamentos/assets/js/alerts.js
+++ 01-fundamentos/assets/js/alerts.js
@@ -1,1 +1,10 @@
 // console.log(miNombre);
+alert('Hola Mundo');
+let nombre = prompt('¿cual es tu nombre?');
+console.log( nombre );
+console.log( '*****'+ nombre + '*****' ); // '' string vacio
+
+const seleccion = confirm('¿Esta seguro de borrar esto?');
+console.log( seleccion );
+
+console.log( global );
