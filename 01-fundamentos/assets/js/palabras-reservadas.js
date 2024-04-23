af6a09323e2d9b35524d8410bd5531b68f516499
jjmuesesq <jjmuesesq@unal.edu.co>
2024-04-23 15:09:17 -0500

    :sparkles: Palabras reservadas y nombre de variables

---
+++ 01-fundamentos/assets/js/palabras-reservadas.js
@@ -0,0 +1,11 @@
+let await = 123;
+//const
+//if
+//enum
+var π = Math.PI;
+let _objeto$ = 123;
+let precio99_99 = 123;
+let jugadorConPuntajeMasAlto = 'JHON'; //camelcase
+class JuegoAnio { //opercamelcase
+
+}
