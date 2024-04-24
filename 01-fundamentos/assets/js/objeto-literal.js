d688681ffc69f9e25d3fa0e02ef427061954f347
jjmuesesq <jjmuesesq@unal.edu.co>
2024-04-24 15:27:24 -0500

    :sparkles: Mas detalles sobre los objetos literales

--- 01-fundamentos/assets/js/objeto-literal.js
+++ 01-fundamentos/assets/js/objeto-literal.js
@@ -1,4 +1,4 @@
-let personaje = {
+const personaje = {
     nombre: 'Tony Stark',
     codeName: 'Ironman',
     vivo: false,
@@ -28,3 +28,29 @@ console.log( 'ultimo traje ', personaje.trajes[personaje.trajes.length - 1 ]);
 const x = 'vivo'
 console.log('Vivo', personaje[x]);
 console.log('ultima pelicula ', personaje["ultima-pelicula"]);
+
+
+// mas detalles
+// eliminar propiedad
+delete personaje.edad;
+console.log( personaje );
+
+//crear nueva propiedad en el objeto
+personaje.casado = true;
+
+//establece las propiedades como elementos de un array
+const entriesPares = Object.entries( personaje );
+console.log( entriesPares );
+
+// personaje = true;
+Object.freeze( personaje );
+
+personaje.dinero = 1000000000;
+personaje.casado = false; // no se puede modificar
+personaje.direccion.ubicacion = 'costa Rica'; // si se puede modificar cuando son objetos y no se asigna el freeze
+console.log(personaje);
+
+//listar las propiedades
+const propiedades = Object.getOwnPropertyNames(personaje);
+const valores = Object.values(personaje);
+console.log(propiedades, valores);
