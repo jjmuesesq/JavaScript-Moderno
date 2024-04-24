207aa46362f2acd987ba8aeec2c0b16c7d98234b
jjmuesesq <jjmuesesq@unal.edu.co>
2024-04-23 22:12:13 -0500

    :sparkles: Arreglos

---
+++ 01-fundamentos/assets/js/arreglos.js
@@ -0,0 +1,27 @@
+// const arr = new Array(10);
+const arr = [10];
+console.log(arr);
+
+let videoJuegos = [ 'Mario 3', 'Megaman', 'Chrono Trigger' ];
+console.log({ videoJuegos });
+console.log( videoJuegos[0] );
+
+let arregloCosas = [
+    true,
+    123,
+    'Jhon',
+    1+2,
+    function(){},
+    ()=>{},
+    {a:1},
+    ['X', 'Megaman', 'Zero', 'Dr. Light', [
+        'Dr. Willy',
+        'Woodman'
+    ]]    
+];
+
+console.log(arregloCosas);
+console.log(arregloCosas[0]);
+console.log(arregloCosas[2]);
+console.log(arregloCosas[7][3]);
+console.log(arregloCosas[7][4][1]);
