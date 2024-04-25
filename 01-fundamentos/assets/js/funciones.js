92135a40f6e530c9658421e799ec0994e3daec26
jjmuesesq <jjmuesesq@unal.edu.co>
2024-04-24 21:02:10 -0500

    :sparkles: Retorno de las funciones

--- 01-fundamentos/assets/js/funciones.js
+++ 01-fundamentos/assets/js/funciones.js
@@ -1,11 +1,15 @@
 // forma tradicional 
 function saludar() {
     console.log('Hola mundo');
+    return 10;
+    // Esto nunca se va a ejecutar
+    console.log('Soy un codigo que esta despues del return');
 }
 
 // forma anonima
 const saludar2 = function() {
     console.log('Hola mundo 2');
+    return [1,2];
 }
 
 // funcion con argumento
@@ -27,9 +31,28 @@ const saludar6 = ( nombre ) => {
 }
 
 saludar();
-saludar();
-saludar2();
+const retornoSaludar = saludar();
+console.log({ retornoSaludar });
+const retornoSaludar2 = saludar2();
+console.log( retornoSaludar2[0], retornoSaludar2[1] );
 saludar3('Jhon', 34, false, 'Colombia');
 saludar4('Jairo');
 saludar5();
 saludar6('Rosy');
+
+const sumar = (a, b) => {
+    return a + b;
+}
+console.log( sumar(1, 2) );
+
+const sumar2 = (a, b) => a + b;
+console.log( sumar2(1, 6) );
+
+function getAleatorio(){
+    return Math.random();
+}
+console.log( getAleatorio() );
+
+//Transformar en funcion de flecha getAleatorio
+const getAleatorio2 = () => Math.random();
+console.log( getAleatorio2() );
