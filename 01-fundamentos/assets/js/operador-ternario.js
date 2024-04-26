17664cc23d58c4382d2e3925fbf0a263af65d50b
jjmuesesq <jjmuesesq@unal.edu.co>
2024-04-26 16:56:29 -0500

    :sparkles: Operador condicional ternario

---
+++ 01-fundamentos/assets/js/operador-ternario.js
@@ -0,0 +1,29 @@
+/*
+* dias de semana abrimos a las 11,
+* pero los fines de semana abrimos a las 9 
+**/
+
+// Entra en un sitio web, para consultar si esta abierto hoy...
+const dia = 1;
+const horaActual = 10;
+
+let horaApertura;
+let mensaje; //Esta abierto, Esta cerrado, hoy abrimos a las X
+// if ( dia === 0 || dia === 6 ) {
+// if ( [0, 6].includes(dia)) {
+//     console.log('Fin de semana');
+//     horaApertura = 9;
+// } else {
+//     console.log('Día de semana');
+//     horaApertura = 11;
+// }
+horaApertura = ( [0, 6].includes(dia) ) ? 9 : 11;
+
+// if ( horaActual >= horaApertura ) {
+//     mensaje = 'Esta abierto';
+// } else {
+//     mensaje = `Esta cerrado, hoy abrimos a las ${ horaApertura }`;
+// }
+mensaje = ( horaActual >= horaApertura) ? 'Esta abierto' : `Esta cerrado, hoy abrimos a las ${ horaApertura }`;
+ 
+console.log({ horaApertura, mensaje });
