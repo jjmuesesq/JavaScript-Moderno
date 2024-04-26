f401e859e92854baed8a9b7c26cbf97764281275
jjmuesesq <jjmuesesq@unal.edu.co>
2024-04-26 11:57:50 -0500

    :sparkles: Laboratorio alternativa al If Else

--- 01-fundamentos/assets/js/if-else.js
+++ 01-fundamentos/assets/js/if-else.js
@@ -20,3 +20,19 @@ if ( dia === 0 ) {
 } else {
     console.log('No es lunes, martes o domingo...');
 }
+
+// sin usar if o switch, unicamente objetos
+dia = 6;
+const diasLetras = {
+    0: 'domingo',
+    1: 'lunes',
+    2: 'martes',
+    3: 'miércoles',
+    4: 'jueves',
+    5: 'viernes',
+    6: 'sábado',
+}
+
+const diasLetras2 = ['domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sabado',];
+console.log( diasLetras[dia] || 'Día no definido');
+console.log( diasLetras2[dia] || 'Día no definido');
