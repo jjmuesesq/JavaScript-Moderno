8c5755c3e7e778a194513336f7c933bf0d90933e
jjmuesesq <jjmuesesq@unal.edu.co>
2024-05-01 00:15:59 -0500

    :sparkles: Valor de cada carta

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -39,4 +39,15 @@ const pedirCarta = () => {
     return carta;
 }
 
-pedirCarta();
+// pedirCarta();
+
+//valor carta
+const valorCarta = ( carta ) => {
+    const valor = carta.substring(0, carta.length - 1);
+    return ( isNaN( valor ) ) ?
+            ( valor === 'A' ) ? 11 : 10
+            : valor * 1;
+}
+
+const valor = valorCarta(pedirCarta());
+console.log({ valor });
