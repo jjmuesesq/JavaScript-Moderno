7b7aedfef54c7f63d97b4b6fb72c93bbf08b237d
jjmuesesq <jjmuesesq@unal.edu.co>
2024-05-05 22:17:47 -0500

    :sparkles: Optimizaciones - Tercera parte

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -1,7 +1,7 @@
 // Patron modulo
 // funcion anonima, autoinvocadas.
 
-(() => {
+const miModulo = (() => {
     'use strict'
     let deck         = [];
     const tipos      = ['C', 'D', 'H', 'S'],
@@ -18,9 +18,17 @@
     // Esta función inicializa el juego
     const inicializarJuego = ( numJugadores = 2 )  => {
         deck = crearDeck();
+        puntosJugadores = [];
         for ( let i = 0; i < numJugadores; i++ ) {
             puntosJugadores.push(0);
         }
+
+
+        puntosHTML.forEach( elem => elem.innerText = 0);
+        divCartasJugadores.forEach( elem => elem.innerHTML = '');
+
+        btnPedir.disabled   = false;
+        btnDetener.disabled = false;
     }
 
     // Esta función crea una nueva baraja
@@ -69,23 +77,8 @@
         divCartasJugadores[turno].append( imgCarta);
     }
 
-    //turno de la computadora
-    const turnoComputadora = ( puntosMinimos ) => {
-        let puntosComputadora = 0;
-        do {
-            const carta = pedirCarta();
-            puntosComputadora = acumularPuntos(carta, puntosJugadores.length - 1);
-            crearCarta( carta, puntosJugadores.length - 1);
-            // const imgCarta = document.createElement('img');
-            // imgCarta.src = `assets/cartas/${ carta }.png`;
-            // imgCarta.classList.add('carta');
-            // divCartasComputadora.append( imgCarta );
-
-            if( puntosMinimos > 21) {
-                break;
-            }
-        } while (( puntosComputadora < puntosMinimos ) && ( puntosMinimos <= 21 ) );
-
+    const determinarGanador = () => {
+        const [puntosMinimos, puntosComputadora] = puntosJugadores;
         setTimeout(() => {
             if( puntosComputadora === puntosMinimos ) {
                 alert('Nadie gana :(');
@@ -97,6 +90,19 @@
                 alert('Computadora gana');
             }
         }, 10);
+    }
+
+    //turno de la computadora
+    const turnoComputadora = ( puntosMinimos ) => {
+        let puntosComputadora = 0;
+        do {
+            const carta = pedirCarta();
+            puntosComputadora = acumularPuntos(carta, puntosJugadores.length - 1);
+            crearCarta( carta, puntosJugadores.length - 1);
+
+        } while (( puntosComputadora < puntosMinimos ) && ( puntosMinimos <= 21 ) );
+        determinarGanador();
+
     }
 
     // Eventos
@@ -121,27 +127,16 @@
     btnDetener.addEventListener( 'click', () => {
         btnPedir.disabled = true;
         btnDetener.disabled = true;
-        turnoComputadora(puntosJugador);
+        turnoComputadora( puntosJugador );
     });
 
-    btnNuevo.addEventListener( 'click', () => {
-        console.clear();
-        inicializarJuego();
-        // deck = [];
-        // deck = crearDeck();
+    // btnNuevo.addEventListener( 'click', () => {
+    //     inicializarJuego();
+    // });
 
-        // puntosJugador     = 0;
-        // puntosComputadora = 0;
-
-        // puntosHTML[0].innerText = 0;
-        // puntosHTML[1].innerText = 0;
-
-        // divCartasComputadora.innerHTML = '';
-        // divCartasJugador.innerHTML = '';
-
-        // btnPedir.disabled   = false;
-        // btnDetener.disabled = false;
-    });
+    return {
+        nuevoJuego: inicializarJuego
+    };
 })();
 
 // (function() {
