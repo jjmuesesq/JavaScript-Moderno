efc425efbd1559249844a34cc42ce8ae5d02b2f6
jjmuesesq <jjmuesesq@unal.edu.co>
2024-05-08 22:11:10 -0500

    :sparkles: Sets y Gets

--- 01-fundamentos/assets/js/classes/class.js
+++ 01-fundamentos/assets/js/classes/class.js
@@ -3,15 +3,25 @@ class Persona {
     nombre = '';
     codigo = '';
     frase  = '';
+    comida = '';
 
     constructor( nombre = 'Sin nombre', codigo = 'Sin codigo', frase = 'Sin frase' ) {
         this.nombre = nombre;
         this.codigo = codigo;
         this.frase  = frase;
     }
+
+    set setComidafavorita( comida ){
+        this.comida = comida.toUpperCase();
+    }
+    get getComidaFavorita(){
+        return `La comida favorita de ${ this.nombre } es ${this.comida}`
+    }
+
     quienSoy(){
         console.log(`Soy ${ this.nombre } y mi identidad es ${ this.codigo }`);
     }
+
     miFrase(){
         this.quienSoy();
         console.log(`${ this.codigo } dice: ${ this.frase }`);
@@ -20,10 +30,14 @@ class Persona {
 
 const spiderman = new Persona('Peter parker', 'Spiderman', 'Soy tu amigable vecino Spyderman');
 const ironman = new Persona('Tony Stark', 'Ironman', 'Yo soy Ironman');
-console.log( spiderman );
-console.log( ironman );
 
-spiderman.quienSoy();
-ironman.quienSoy();
+// console.log( ironman );
+
+// spiderman.quienSoy();
+// ironman.quienSoy();
 spiderman.miFrase();
-ironman.miFrase();
+// ironman.miFrase();
+
+spiderman.setComidafavorita = 'El pie de cereza de la tía May';
+console.log( spiderman.getComidaFavorita );
+console.log( spiderman );
