b3a04cac7fa8e4fe1e8b3d2e363f98c19f7f7a5e
jjmuesesq <jjmuesesq@unal.edu.co>
2024-05-08 21:44:21 -0500

    :sparkles: Métodos en las clases

--- 01-fundamentos/assets/js/classes/class.js
+++ 01-fundamentos/assets/js/classes/class.js
@@ -9,9 +9,21 @@ class Persona {
         this.codigo = codigo;
         this.frase  = frase;
     }
+    quienSoy(){
+        console.log(`Soy ${ this.nombre } y mi identidad es ${ this.codigo }`);
+    }
+    miFrase(){
+        this.quienSoy();
+        console.log(`${ this.codigo } dice: ${ this.frase }`);
+    } 
 }
 
-const spiderman = new Persona('Peter parker', 'Spider', 'Soy tu amigable vecino Spyderman');
+const spiderman = new Persona('Peter parker', 'Spiderman', 'Soy tu amigable vecino Spyderman');
 const ironman = new Persona('Tony Stark', 'Ironman', 'Yo soy Ironman');
 console.log( spiderman );
 console.log( ironman );
+
+spiderman.quienSoy();
+ironman.quienSoy();
+spiderman.miFrase();
+ironman.miFrase();
