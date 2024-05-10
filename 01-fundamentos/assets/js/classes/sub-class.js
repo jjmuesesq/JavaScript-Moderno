edae3a57b436cff1ddb5b20eca68c3abeb36ca4e
jjmuesesq <jjmuesesq@unal.edu.co>
2024-05-09 21:56:37 -0500

    :sparkles: Extends - Clases con subClases

---
+++ 01-fundamentos/assets/js/classes/sub-class.js
@@ -0,0 +1,62 @@
+class Persona {
+    //inicializacion propiedades de una clase
+    static _conteo = 0;
+    static get conteo(){
+        return Persona._conteo + ' instancias';
+    }
+    static mensaje(){
+        console.log( this.nombre ); //undefined
+        console.log('Hola a todos, soy un método stático');
+    }
+
+    nombre = '';
+    codigo = '';
+    frase  = '';
+    comida = '';
+
+    constructor( nombre = 'Sin nombre', codigo = 'Sin codigo', frase = 'Sin frase' ) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.frase  = frase;
+
+        Persona._conteo++;
+    }
+
+    set setComidafavorita( comida ){
+        this.comida = comida.toUpperCase();
+    }
+    get getComidaFavorita(){
+        return `La comida favorita de ${ this.nombre } es ${this.comida}`
+    }
+
+    quienSoy(){
+        console.log(`Soy ${ this.nombre } y mi identidad es ${ this.codigo }`);
+    }
+
+    miFrase(){
+        this.quienSoy();
+        console.log(`${ this.codigo } dice: ${ this.frase }`);
+    } 
+}
+
+
+// Extender una clase
+class Heroe extends Persona{
+
+    clan = 'Sin clan';
+
+    constructor(nombre, codigo, frase){
+        super(nombre, codigo, frase);
+        this.clan = 'Los Avengers';
+    }
+
+    quienSoy(){
+        console.log(`Soy ${ this.nombre }, ${ this.clan }`);
+        super.quienSoy();
+    }
+}
+
+const spiderman = new Heroe('Peter parker', 'Spiderman', 'Soy tu amigable vecino Spyderman');
+// const spiderman = new Heroe();
+console.log( spiderman );
+spiderman.quienSoy();
