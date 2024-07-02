f1252bb70ffa6b21730d15838ad4b90dab03efba
jjmuesesq <jjmuesesq@unal.edu.co>
2024-07-02 11:33:58 -0500

    :sparkles: Metodos que ocuparemos del Store

--- 05-todo-app/src/store/todo.store.js
+++ 05-todo-app/src/store/todo.store.js
@@ -20,6 +20,47 @@ const initStore = () => {
     console.log('InitStore ..');
 }
 
+const loadStore = () => {
+    throw new Error('Not implemented');
+}
+
+/**
+ *  @param {String} description 
+ */
+const addTodo = ( description ) => {
+    throw new Error('Not implemented');
+}
+
+/**
+ *  @param {String} todoId Todo identifier
+ */
+const toggleTodo = ( todoId ) => {
+    throw new Error('Not implemented');
+}
+
+const deleteTodo = ( todoId ) => {
+    throw new Error('Not implemented');
+}
+
+const deleteCompleted = () => {
+    throw new Error('Not implemented');
+}
+
+const setFilter = ( newFilter = Filters.All ) => {
+    throw new Error('Not implemented');
+}
+
+const getCurrentFilter = () => {
+    throw new Error('Not implemented');
+}
+
 export default {
     initStore,
+    loadStore,
+    addTodo,
+    toggleTodo,
+    deleteTodo,
+    deleteCompleted,
+    setFilter,
+    getCurrentFilter,
 }
