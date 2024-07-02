0910cabf7e848d63a9ea3e1db78b44cb10bfb372
jjmuesesq <jjmuesesq@unal.edu.co>
2024-07-02 12:36:00 -0500

    :sparkles: Implementar metodos del store

--- 05-todo-app/src/store/todo.store.js
+++ 05-todo-app/src/store/todo.store.js
@@ -24,34 +24,56 @@ const loadStore = () => {
     throw new Error('Not implemented');
 }
 
+const getTtodo = ( filter = Filters.All ) => {
+    
+    switch(filter) {
+        case Filters.All:
+            return [...state.todos]; // regresar un nuevo arreglo de Todos con el operador spret
+
+        case Filters.Completed:
+            return state.todos.filter( todo => todo.done); //regresa todo si es true
+        
+        case Filters.Pending:
+            return state.todos.filter( todo => !todo.done); //regresa todo si es falso
+
+        default:
+            throw new Error(`Option ${ filter } is not valid`);
+    }
+}
+
 /**
  *  @param {String} description 
  */
 const addTodo = ( description ) => {
-    throw new Error('Not implemented');
+    if (!description) throw new Error('Description is required');
+    state.todos.push( new Todo(description) );
 }
 
 /**
- *  @param {String} todoId Todo identifier
+ *  @param {String} todoId
  */
 const toggleTodo = ( todoId ) => {
     throw new Error('Not implemented');
 }
 
 const deleteTodo = ( todoId ) => {
-    throw new Error('Not implemented');
+    state.todos = state.todos.filter( todo => todo.id !== todoId);
 }
 
 const deleteCompleted = () => {
-    throw new Error('Not implemented');
+    state.todos = state.todos.filter( todo => todo.done );
 }
 
+/**
+ * 
+ * @param { Filters } newFilter 
+ */
 const setFilter = ( newFilter = Filters.All ) => {
-    throw new Error('Not implemented');
+    state.filter = newFilter;
 }
 
 const getCurrentFilter = () => {
-    throw new Error('Not implemented');
+    return state.filter;
 }
 
 export default {
@@ -63,4 +85,5 @@ export default {
     deleteCompleted,
     setFilter,
     getCurrentFilter,
+    getTtodo
 }
