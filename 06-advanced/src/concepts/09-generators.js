8815771f41816a8c4097514eed684a52f395d12a
jjmuesesq <jjmuesesq@unal.edu.co>
2024-07-12 02:05:12 -0500

    :sparkles: Ejemplo de funcion generadora

--- 06-advanced/src/concepts/09-generators.js
+++ 06-advanced/src/concepts/09-generators.js
@@ -5,12 +5,33 @@
 
 export const generatorFunctionsComponent = (element) => {
     console.log('generatorFunctionComponent');
-    const myGenerator = myFirstGeneratorFunction();
-    console.log(myGenerator.next());
-    console.log(myGenerator.next());
-    console.log(myGenerator.next());
-    console.log(myGenerator.next());
-    console.log(myGenerator.next());
+    // const myGenerator = myFirstGeneratorFunction();
+    // console.log(myGenerator.next());
+    // console.log(myGenerator.next());
+    // console.log(myGenerator.next());
+    // console.log(myGenerator.next());
+    // console.log(myGenerator.next());
+
+    const genId = idGenerator();
+    // console.log(genId.next());
+    const button = document.createElement('button');
+    button.innerText = 'Click me';
+    element.append(button);
+
+    const renderButton = () => {
+        const {value} = genId.next();
+        button.innerText = `Click ${ value }`;
+    }
+
+    // button.addEventListener('click', (event) => renderButton());
+    button.addEventListener('click', renderButton);
+}
+
+function* idGenerator() {
+    let currentId = 0;
+    while(true){
+        yield ++currentId;
+    }
 }
 
 function* myFirstGeneratorFunction () {
