367767ea11f157bc1ad890f68f49a0c9d13993cf
jjmuesesq <jjmuesesq@unal.edu.co>
2024-07-11 23:36:09 -0500

    :sparkles: Optimizar Promesas no secuenciales

--- 06-advanced/src/concepts/06-async-await.js
+++ 06-advanced/src/concepts/06-async-await.js
@@ -10,7 +10,7 @@ export const asyncAwaitComponent = async(element) => {
     const id1 = '5d86371f1efebc31def272e22';
     const id2 = '5d86371f25a058e5b1c8a65e';
     try {
-        const hero1 = await findHero( id1);
+        const hero1 = await findHero( id1); // promesas no secuenciales, un valor no depende del otro
         const hero2 = await findHero( id2);
         element.innerHTML = `${hero1.name} / ${hero2.name}`;
 
