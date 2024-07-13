f68b7af7e56d7512903714ec1e1a0608e6b9ab4a
jjmuesesq <jjmuesesq@unal.edu.co>
2024-07-13 03:52:06 -0500

    :sparkles: Mostrar quote en pantalla

--- 07-http-app/src/breakingbad/breakingbad-app.js
+++ 07-http-app/src/breakingbad/breakingbad-app.js
@@ -17,6 +17,20 @@ export const BreakingbadApp = async(element ) => {
     document.querySelector('#app-title').innerHTML = 'Breakingbad App'
     element.innerHTML = 'Loading...';
     
-    const quote = await fetchQoute();
-    element.innerHTML = 'Tenemos data!!';
+    // const quote = await fetchQoute();
+    // element.innerHTML = 'Tenemos data!!';
+
+    const quoteLabel = document.createElement('blockquote');
+    const authorLabel = document.createElement('h3');
+    const nextQuoteButton = document.createElement('button');
+    nextQuoteButton.innerText = 'Next Quote';
+
+    const renderQuote = (data) => {
+        quoteLabel.innerHTML = data.quote;
+        authorLabel.innerHTML = data.author;
+        element.replaceChildren(quoteLabel, authorLabel, nextQuoteButton);
+    }
+
+    fetchQoute()
+        .then(renderQuote);
 }
