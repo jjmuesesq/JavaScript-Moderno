4e875759f475cae58272a7d95cdb882b576707c4
jjmuesesq <jjmuesesq@unal.edu.co>
2024-07-13 03:39:29 -0500

    :sparkles: FetchApi - peticion http get

--- 07-http-app/src/breakingbad/breakingbad-app.js
+++ 07-http-app/src/breakingbad/breakingbad-app.js
@@ -1,9 +1,22 @@
 /**
+ * @returns {Promise<Object>} quote information 
+ */
+const fetchQoute = async() => {
+    const res = await fetch('https://api.breakingbadquotes.xyz/v1/quotes');
+    const data = await res.json();
+    console.log(data[0]);
+    return data[0];
+}
+
+/**
  * 
  * @param {HTMLDivElement} element 
  */
-export const BreakingbadApp = (element ) => {
+export const BreakingbadApp = async(element ) => {
     // console.log('Hola mundo');
     document.querySelector('#app-title').innerHTML = 'Breakingbad App'
-    element.innerHTML = 'Loading...'
+    element.innerHTML = 'Loading...';
+    
+    const quote = await fetchQoute();
+    element.innerHTML = 'Tenemos data!!';
 }
