a61016def681fafd3d51dc5213ffb86183778d97
jjmuesesq <jjmuesesq@unal.edu.co>
2024-07-13 02:15:16 -0500

    :sparkles: Breakingbad component

---
+++ 07-http-app/src/breakingbad/breakingbad-app.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const BreakingbadApp = (element ) => {
+    // console.log('Hola mundo');
+    document.querySelector('#app-title').innerHTML = 'Breakingbad App'
+    element.innerHTML = 'Loading...'
+}
