18c7c2b1a605f842bfc05c78279c93923d3e01ee
jjmuesesq <jjmuesesq@unal.edu.co>
2024-07-17 09:37:46 -0500

    :sparkles: Boton flotante

---
+++ 07-http-app/src/users/presentation/render-add-button/render-add-button.js
@@ -0,0 +1,25 @@
+import './render-add-button.css';
+/**
+ * 
+ * @param {HTMLDivElement} element 
+//  * @param {() => void} callback
+ */
+
+export const renderAddButton = (element, callback) => {
+    const fabButton = document.createElement('button');
+    fabButton.innerText = '+';
+    fabButton.classList.add('fab-button');
+
+    element.append( fabButton );
+
+    //TODO
+    // fabButton.addEventListener('click', () => {
+    //     throw Error('No implementado');
+    //     if( !callback ) return;
+    //     callback();
+    // });
+
+    fabButton.addEventListener('click', () => {
+        throw Error('No implementado');
+    });
+}
--- 07-http-app/src/users/users-app.js
+++ 07-http-app/src/users/users-app.js
@@ -1,3 +1,4 @@
+import { renderAddButton } from './presentation/render-add-button/render-add-button';
 import { renderButtons } from './presentation/render-buttons/render-buttons';
 import { renderTable } from './presentation/render-table/render-table';
 import usersStore from './store/users-store';
@@ -12,4 +13,6 @@ export const UsersApp = async(element) => {
     element.innerHTML = '';
     renderTable(element);
     renderButtons(element);
+    // renderAddButton(element, () => { console.log('desde el padre')} ); //el padre delega al hijo que hacer en el callback
+    renderAddButton(element);
 }
