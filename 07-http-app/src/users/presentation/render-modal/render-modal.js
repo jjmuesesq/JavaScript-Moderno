a164c65bd2613cd6cd505f675729b4bce197168e
jjmuesesq <jjmuesesq@unal.edu.co>
2024-07-18 17:37:23 -0500

    :sparkles: Tomar la data del formulario

--- 07-http-app/src/users/presentation/render-modal/render-modal.js
+++ 07-http-app/src/users/presentation/render-modal/render-modal.js
@@ -9,7 +9,7 @@ export const showModal = () => {
 
 export const hideModal = () => {
     modal?.classList.add('hide-modal');
-    //TODO reset del formulario
+    form?.reset();
 }
 
 /**
@@ -37,7 +37,29 @@ export const renderModal = (element) => {
         //prevenir el comportamiento por defecto
 
         event.preventDefault();
-        console.log('Formulario enviado');
+        // console.log('Formulario enviado');
+        const formData = new FormData( form );
+        const userLike = {};
+
+        for (const [key, value] of formData) {
+            if(key === 'balance'){
+                userLike[key] = +value; //conversion en balance a numerico
+                continue;
+            }
+
+            if(key === 'isActive'){
+                userLike[key] = (value === 'on') ? true : false; //conversion isActive a booleano
+                continue;
+            }
+
+            userLike[key] = value;
+
+        }
+
+        // console.log(userLike);
+        //TODO guardar usuario
+        hideModal();
+
     });
 
     element.append(modal);
